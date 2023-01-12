b9bae674034b2c313b376ccec895d785d0a88fdc
thomnstuff <thomness03@gmail.com>
2023-01-11 20:15:20 -0800

    mod date added

---
+++ PortalPage/scripts/app.js
@@ -0,0 +1,9 @@
+const dyear = document.querySelector("#currentyear");
+
+dyear.innerHTML = 2023;
+
+let text = document.lastModified;
+document.querySelector("#currentdate").innerHTML = `Last Updated: ${text}`;
+
+
+
