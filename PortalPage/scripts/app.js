063c1ee5e5f9393e7da2139fa7994ff72c21f6ba
thomnstuff <thomness03@gmail.com>
2023-01-12 18:07:21 -0800

    Made fixes to file/id names and centered content

--- PortalPage/scripts/app.js
+++
@@ -1,9 +0,0 @@
-const dyear = document.querySelector("#currentyear");
-
-dyear.innerHTML = 2023;
-
-let text = document.lastModified;
-document.querySelector("#currentdate").innerHTML = `Last Updated: ${text}`;
-
-
-
---
+++ portal-page/scripts/app.js
@@ -0,0 +1,9 @@
+const dyear = document.querySelector("#current-year");
+
+dyear.innerHTML = 2023;
+
+let text = document.lastModified;
+document.querySelector("#current-date").innerHTML = `Last Updated: ${text}`;
+
+
+
