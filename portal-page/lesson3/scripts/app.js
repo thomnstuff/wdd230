62492650026dbdb12c4d17684f559068d56453f2
thomnstuff <thomness03@gmail.com>
2023-01-21 16:24:44 -0800

    lesson3 commerce

---
+++ portal-page/lesson3/scripts/app.js
@@ -0,0 +1,6 @@
+const dyear = document.querySelector("#current-year");
+
+dyear.innerHTML = 2023;
+
+let text = document.lastModified;
+document.querySelector("#current-date").innerHTML = `Last Updated: ${text}`;
