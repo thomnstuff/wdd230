957fd3cb38c56234f7506c3f9eed719e1eba872b
thomnstuff <thomness03@gmail.com>
2023-01-24 21:38:10 -0800

    fix the fix

--- portal-page/scripts/app.js
+++ portal-page/scripts/app.js
@@ -1,6 +1,6 @@
-const dyear = document.querySelector("#current-year");
+const dYear = document.querySelector("#current-year");
 
-dyear.innerHTML = 2023;
+dYear.innerHTML = 2023;
 
 let text = document.lastModified;
 document.querySelector("#current-date").innerHTML = `Last Updated: ${text}`;
